9f85a51335a386e8cf98680794ed875bf971b161
Fabrizzio Lora <43654747-snxwsxulles@users.noreply.replit.com>
2025-06-10 15:11:51 +0000

    aumentando mas aun.........

---
+++ factorizar.py
@@ -0,0 +1,20 @@
+# Rectangulo 1 
+base1 = 10
+altura1 = 5
+area1 = base1 * altura1
+print(f"El área del rectángulo 1 ({base1}x{altura1}) es: {area1}")
+
+# Rectangulo 2
+base2 = 7
+altura2 = 3
+area2 = base2 * altura2
+    print(f"El área del rectángulo 2 ({base2}x{altura2}) es: {area2}")
+
+
+          def mostrar_area_rectangulo(numero, base, altura):
+                area = calcular_area_rectangulo (base, altura)
+                print(f"El área del rectángulo {numero} es: ({base}x{altura}) es: {area}")
+
+            #ahora:1 linea por rectangulo
+            mostrar_area_rectangulo(1, 10, 5)
+            
