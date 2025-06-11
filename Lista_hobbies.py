52ea05ca7fcf0c9957bc37044bbfaba5282398de
Fabrizzio Lora <43654747-snxwsxulles@users.noreply.replit.com>
2025-06-11 12:35:11 +0000

    aumentando listas

---
+++ Lista_hobbies.py
@@ -0,0 +1,5 @@
+lista_hobbies = ["Leer", "Caminar", "Programar", "Jugar videojuegos", "Ver películas"]
+print(f"mis hobbies son: {', '.join(lista_hobbies)}.")
+
+lista_comidas_favoritas = ["Pizza", "Hamburguesas", "Sushi", "Tacos", "Ensaladas"]
+print(f"mis comidas favoritas son: {', '.join(lista_comidas_favoritas)}.")
