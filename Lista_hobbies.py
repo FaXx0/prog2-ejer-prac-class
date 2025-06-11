3f1ec63e6a09872f1a10638460cc9a6920721d55
Fabrizzio Lora <43654747-snxwsxulles@users.noreply.replit.com>
2025-06-11 12:58:39 +0000

    aumentando listas CON FAVORITOS

--- Lista_hobbies.py
+++ Lista_hobbies.py
@@ -1,5 +1,10 @@
 lista_hobbies = ["Leer", "Caminar", "Programar", "Jugar videojuegos", "Ver películas"]
+Favoritos = ["programar","leer", "jugar videojuegos"]
 print(f"mis hobbies son: {', '.join(lista_hobbies)}.")
-
+print(f"mis favoritos: {', '.join(Favoritos)}.")
+################################################################################
+################################################################################
 lista_comidas_favoritas = ["Pizza", "Hamburguesas", "Sushi", "Tacos", "Ensaladas"]
+Favoritos = ["Pizza", "Sushi", "Ensaladas"]
 print(f"mis comidas favoritas son: {', '.join(lista_comidas_favoritas)}.")
+print(f"mis favoritos: {', '.join(Favoritos)}.")
