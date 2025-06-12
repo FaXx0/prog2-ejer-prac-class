45634baabc4fdb7c7148557d92f42ce2daed486a
Fabrizzio Lora <43654747-snxwsxulles@users.noreply.replit.com>
2025-06-12 12:00:50 +0000

    cambiando a solicitud del inge el join por el for

--- Lista_hobbies.py
+++ Lista_hobbies.py
@@ -1,20 +1,27 @@
 lista_hobbies = ["Leer", "Caminar", "Programar", "Jugar videojuegos", "Ver películas"]
+for lista_hobbies in lista_hobbies:
+   print(f"Mis hobbies son : {lista_hobbies}")
+#print(f"Mis hobbies son: {', '.join(lista_hobbies)}.")
+#print(f"Mis favoritos: {', '.join(Favoritos)}.")
 Favoritos = ["Programar", "Leer", "Jugar videojuegos"]
-
-print(f"Mis hobbies son: {', '.join(lista_hobbies)}.")
-print(f"Mis favoritos: {', '.join(Favoritos)}.")
 print("MI PRIMER HOBBIE FAVORITO ES: " + Favoritos[0])
 print("MI SEGUNDO HOBBIE FAVORITO ES: " + Favoritos[1])
 print("MI TERCER HOBBIE FAVORITO ES: " + Favoritos[2])
+for hobbies_favoritos in Favoritos:
+    print(f"Mis hobbies favoritos son :{hobbies_favoritos}")
 
 
 ################################################################################
 ################################################################################
 Comidas_favoritas = ["Pizza", "Hamburguesas", "Sushi", "Majadito", "Ensaladas"]
+for Comida_favorita in Comidas_favoritas:
+  print (f"Mis comidas favoritas son: {Comida_favorita}")
+  
 Favoritos = ["Pizza", "Sushi", "Ensaladas"]
-
-print(f"Mis comidas favoritas son: {', '.join(Comidas_favoritas)}.")
-print(f"Mis favoritas seleccionadas son: {', '.join(Favoritos)}.")
+#print(f"Mis comidas favoritas son: {', '.join(Comidas_favoritas)}.")
+#print(f"Mis favoritas seleccionadas son: {', '.join(Favoritos)}.")
 print("MI PRIMER COMIDA FAVORITA ES: " + Favoritos[0])
 print("MI SEGUNDA COMIDA FAVORITA ES: " + Favoritos[1])
 print("MI TERCER COMIDA FAVORITA ES: " + Favoritos[2]) 
+for comidas_favoritas in Favoritos:
+    print(f"Mis comidas favoritas son :{comidas_favoritas}")
